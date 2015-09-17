d11d31f28201f3c6fa7314388a1ec40947ea27dd
jlima2016 <jlima2016@pingry.org>
2015-09-17 14:10:08 -0400

    branched data structure
    pls work

--- MyDataStructure.java
+++ MyDataStructure.java
@@ -1,6 +1,16 @@
 public class MyDataStructure
 {
+	private String feelings;
 	
-
-
+	public void eat() {
+		feelings = "good";
+	}
+	
+	public void sleep() {
+		feelings = "good";
+	}
+	
+	public void goToSchool() {
+		feelings = "tired and hungry";
+	}
 }
