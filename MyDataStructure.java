910373588b4953de864b306f4d0308ea58dbe607
wfallon <wfallon2018@pingry.org>
2015-09-17 14:14:58 -0400

    MyDataStructure
    greatest datastructure of all time no big deal

--- MyDataStructure.java
+++ MyDataStructure.java
@@ -1,6 +1,31 @@
 public class MyDataStructure
 {
+	private int anything;
+	private String something;
+	private double thing;
 	
-
-
+	public MyDataStructure(int a, String b, double c)
+	{
+		anything=a;
+		something=b;
+		thing = c;
+	}
+	
+	public MyDataStructure(int a, double c)
+	{
+		anything = a;
+		something = "Billy";
+		thing = c;
+	}
+	
+	public void changeAnything(int a)
+	{
+		anything = a;
+	}
+	
+	public String getSomething ()
+	{
+		return something;
+	}
+	
 }
