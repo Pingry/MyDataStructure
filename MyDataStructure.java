0df1978d4f95a8f880150ec8a715cfe6efdcc688
wlong123 <wlong2017@pingry.org>
2015-09-17 14:22:12 -0400

    Update MyDataStructure.java

--- MyDataStructure.java
+++ MyDataStructure.java
@@ -1,6 +1,31 @@
 public class MyDataStructure
 {
+	private int age;
+	private String name;
+	private String gender;
+	public MyDataStructure()
+	{
+		age = 16;
+		name = "Will";
+		gender = "male";
+	}
 	
+	public MyDataStructure(int a, String n, String g)
+	{
+		age = a;
+		name = n;
+		gender = g;
+	}
+	
+	public int getAge()
+	{
+		return age;
+	}
+	
+	public int setAge(int a)
+	{
+		age = a;
+	}	
 
 
 }
