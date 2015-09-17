eb1e9c5de68e89c92d79df99ea926e278a429305
aseth2017 <aseth2017@pingry.org>
2015-09-17 14:20:07 -0400

    Update MyDataStructure.java

--- MyDataStructure.java
+++ MyDataStructure.java
@@ -1,6 +1,35 @@
 public class MyDataStructure
 {
+	private int age;
+	private String name, gender;
+	private boolean Hunger, Thirst, Tired, Pain, Dead;
 	
+	public myDataStructure(String name, String gender, boolean Hunger, boolean Thirst, boolean Tired, boolean Pain, boolean Dead, int age)
+	{
+		this.name = name;
+		this.gender = gender;
+		this.Hunger = Hunger;
+		this.Thirst = Thirst;
+		this.Tired = Tired;
+		this.Pain = Pain;
+		this.Dead = Dead;
+	}
+	public String getName()
+	{
+		return name;
+	}
+	public int getAge()
+	{
+		return age;
+	}
+	public String getGender()
+	{
+		return gender;
+	}
+	public boolean getHunger()
+	{
+		return Hunger;
+	}
 
 
 }
