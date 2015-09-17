427ca777882240ca719e2f37d4629e7f038125bb
kchow98 <kchow2016@pingry.org>
2015-09-17 14:24:37 -0400

    does this work
    please

--- MyDataStructure.java
+++ MyDataStructure.java
@@ -1,6 +1,17 @@
 public class MyDataStructure
 {
+	private String name;
+	private int age;
+	private string favbook;
 	
+	public MyDataStructure( String n, int a, String f)
+	{
+		name = n
+		age = a
+		favebook  = f
+	}
+	
+
 
 
 }
