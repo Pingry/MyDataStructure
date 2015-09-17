67fe5c216ccc2873c43dd73b7397c779a917b769
jproudfoot <jproudfoot2018@pingry.org>
2015-09-17 14:16:25 -0400

    Update MyDataStructure.java

--- MyDataStructure.java
+++ MyDataStructure.java
@@ -1,6 +1,24 @@
 public class MyDataStructure
 {
+	private String firstName, middleName, lastName, title;
+	private int age, height, weight;
+	private boolean isHungry, isTired;
+	private String gender;
 	
+	
+	public static MyDataStructure (String firstName, String middleName, String lastName, String title, int age, int height, int weight, boolean isHungry, boolean isTired, String gender) {
+		this.firstName = firstName;
+		this.middleName = middleName;
+		this.lastName = lastName;
+		this.title = title;
+		this.age = age;
+		this.height = height;
+		this.weight = weight;
+		this.isHungry = isHungry;
+		this.isTired = isTired;
+		this.gender = gender;
+		
+	}
 
 
 }
