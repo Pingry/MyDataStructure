0b97592dfd52aae2570c50ef71e305d19de5e714
Zachary Keller <zkeller2017@pingry.org>
2015-09-17 14:23:29 -0400

    My first attempt, not finished

--- MyDataStructure.java
+++ MyDataStructure.java
@@ -1,5 +1,37 @@
 public class MyDataStructure
 {
+
+	private int age;
+	private String title;
+	private boolean isAlive;
+	
+	public MyDataStructure()
+	{
+		age=15;
+		title= "Master Professor";
+		isAlive= true;
+		
+	}
+	
+	public void setAge(int a)
+	{
+		age=a;
+	}
+	
+	public void setTitle(String a)
+	{
+		title=a;
+	}
+	
+	public void setLife(int a)
+	{
+		isAlive=a;
+	}
+	
+	public int getAge()
+	{
+		return age:
+	}
 	
 
 
